a68283e33a0121200958eebcab9a7633e0dc1498
eempc <evc484@gmail.com>
2019-11-12 19:29:40 +0000

    first

---
+++ WebApplication1/Areas/Identity/Data/WebApplication1User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication1.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebApplication1User class
+    public class WebApplication1User : IdentityUser
+    {
+        // These two new fields are added here
+        [PersonalData]
+        public string Name { get; set; }
+        [PersonalData]
+        public DateTime DOB { get; set; }
+    }
+}
